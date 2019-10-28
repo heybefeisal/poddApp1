1eb90bd3863d80ccdded9bbe0b4275505f4db67a
Heybe Feisal <46941322+heybefeisal@users.noreply.github.com>
2019-10-29 00:28:02 +0100

    Senaste version

--- poddApp11/poddApp11/BLL/Kategori.cs
+++ poddApp11/poddApp11/BLL/Kategori.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using poddAppen.DataLager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace poddAppen.BLL
 {
-    class Kategori
+    class Kategori : Podden
     {
+        public List<string> KategoriLista = new List<string>();
+
+        SparaXML sparaXML = new SparaXML();
+
+
+        public void LaggTillKatXML(ListView list)
+        {
+            LaddaXML laddaXML = new LaddaXML();
+            laddaXML.LaddaKategori(KategoriLista);
+
+            foreach (var kategori in KategoriLista)
+            {
+                base.laggTill(list, kategori);
+            }
+        }
+
+        public override void laggTill(ListView list, string objekt)
+        {
+            string kategori = objekt;
+            KategoriLista.Add(kategori);
+            sparaXML.sparaKat(KategoriLista);
+            base.laggTill(list, kategori);
+        }
+
+        public override void taBort(ListView list)
+        {
+            KategoriLista.Remove(list.SelectedItems[0].Text);
+            sparaXML.sparaKat(KategoriLista);
+            base.taBort(list); 
+        }
+
+        public override void sparaAndring(ListView listViewKategori, TextBox textBox)
+        {
+            string nyKategori = textBox.Text;
+            KategoriLista.Remove(listViewKategori.SelectedItems[0].Text);
+            KategoriLista.Add(nyKategori);
+            sparaXML.sparaKat(KategoriLista);
+            base.sparaAndring(listViewKategori, textBox);
+        }
+
     }
 }
+
--- poddApp11/poddApp11/DL/TaBortXML.cs
+++ poddApp11/poddApp11/DL/TaBortXML.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace poddAppen.DataLager
 {
     class TaBortXML
     {
+        public void taBortPodd(string urlKod, string uppdatering, string kategori)
+        {
+            XDocument dokument = XDocument.Load("Podd.xml");                // XDocument rep en XDocumentKlass. 
+            var pod = from node in dokument.Descendants("Pod")              // från varje elemement i XDoucument klass Pod
+                      let UrlKod = node.Element("UrlKod")                             // let används för att ställa frågan flera ggr
+                      let Uppdatering = node.Element("Uppdatering")
+                      let Kategori = node.Element("Kategori")
+                      where UrlKod != null && UrlKod.Value == urlKod                  // kollar så att det inte är null och att det stämmer med parameter värdet
+                      where Uppdatering != null && Uppdatering.Value == uppdatering
+                      where Kategori != null && Kategori.Value == kategori
+                      select node;                                                    // välj denna där detta stämmer
+            pod.ToList().ForEach(xmlList => xmlList.Remove());              // För varje podd i listan som dettta stämmer. ta bort elementet
+            dokument.Save("Podd.xml")                                       // spara uppdateringen i listan. 
+        }
+
     }
 }
