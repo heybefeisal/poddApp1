d62a235879d831e2e52cf76c19f6a51b763171c4
Heybe Feisal <46941322+heybefeisal@users.noreply.github.com>
2019-10-29 14:49:40 +0100

    Gränsnitt

--- poddApp11/poddApp11/BLL/ValideraMeddelanden.cs
+++ poddApp11/poddApp11/BLL/ValideraMeddelanden.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace poddAppen.BLL
 {
-    class ValideraMeddelanden
+    public class ValideraMeddelanden
     {
+        public void IngetVald()
+        {
+            MessageBox.Show("Markera den du vill ta-bort/ändra");
+        }
+
+        public void tomUrl()
+        {
+            MessageBox.Show("Fyll i url fältet");
+        }
+
+        public void giltigUrl()
+        {
+            MessageBox.Show("Fyll i giltig Rss-url!");
+        }
+
+        public void urlFinnsRedan()
+        {
+            MessageBox.Show("Podd url har redan lagts till");
+        }
+
+        public void tomFrekvens()
+        {
+            MessageBox.Show("Ange uppdateringsintervall");
+        }
+
+        public void tomKategori()
+        {
+            MessageBox.Show("Tom kategori fält");
+        }
     }
 }
